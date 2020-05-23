d3e0b7fadf0c426c46d94ea21d64911cc882a438
Kevin Goodrich <3819200+krgoodrich07@users.noreply.github.com>
2020-05-22 21:23:20 -0500

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -6,19 +6,19 @@ from .models import Greeting
 import requests
 import os
 
-## Create your views here.
-#def index(request):
-#    # return HttpResponse('Hello from Python!')
-#    return render(request, "index.html")
+# Create your views here.
+def index(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, "index.html")
 
 #1def index(request):
 #1    r = requests.get('http://httpbin.org/status/418')
 #1    print(r.text)
 #1    return HttpResponse('<pre>' + r.text + '</pre>')
 
-def index(request):
-    times = int(os.environ.get('TIMES',3))
-    return HttpResponse('Hello! ' * times)
+#def index(request):
+#    times = int(os.environ.get('TIMES',3))
+#    return HttpResponse('Hello! ' * times)
 
 def db(request):
 
